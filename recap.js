f6824aed399c625c14d26b89edffca64896144df
noman-swe <noman.swe130@gmail.com>
2022-08-21 23:24:54 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,15 @@
+// 1. let /const
+// 2.carret usages --chailei string er moddhy variable set kora possible hocchy
+// 3.default parameter
+
+// 
+function maxNumber(array){
+    const max = Math.max(...array);
+    return max;
+}
+
+const max = maxNumber([45, 56, 32, 89]);
+console.log(max);
+
+
+// arrow f
