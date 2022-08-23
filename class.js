09b8bd0c3d3155979bc89f17dc27a9775812ef14
noman-swe <noman.swe130@gmail.com>
2022-08-24 01:07:13 +0600

    32-8 (advanced) Inheritance, extends class, super, class method

--- class.js
+++ class.js
@@ -2,16 +2,16 @@ class Support {
     name;
     designation = 'support web dev';
     address;
-    constructor(name, address){
+    constructor(name, address) {
         this.name = name;
         this.address = address;
     }
-    startSession(){
+    startSession() {
         console.log(this.name, 'start a support session');
     }
 }
 
-const amirKhan = new Support('Amir', 'Dhaka'); 
+const amirKhan = new Support('Amir', 'Dhaka');
 const salman = new Support('Salman', 'Tangail'); //this returs a object
 console.log(amirKhan);
 console.log(salman);
---
+++ inheritance.js
@@ -0,0 +1,64 @@
+
+class TeamMember{
+    name;
+    designation = 'support web dev';
+    address;
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+}
+
+
+class Support extends TeamMember {
+    
+    startSession() {
+        console.log(this.name, 'start a support session');
+    }
+}
+
+
+// another class for student care
+class StudentCare extends TeamMember{
+   groupSupportTime;
+   constructor(name, address, time){
+    super(name, address)
+    this.groupSupportTime = time;
+   }
+    giveARoutine(student){
+        console.log(this.name, 'Build a routine for', student);
+    }
+}
+
+
+////
+// another class for Naptune for realise app
+class NaptuneDev extends TeamMember{
+    codeEditor;
+    constructor(name, address, editor){
+        super(name, address);
+
+        this.codeEditor = editor;
+    }
+    realiseApp(version){
+        console.log(this.name, 'Naptune app realise--', version);
+    }
+}
+
+
+
+const akin = new StudentCare('Akin', 'Tangail', 11);
+akin.giveARoutine('Sakib');
+console.log(akin);
+
+const amirKhan = new Support('Amir', 'Dhaka');
+const salman = new Support('Salman', 'Tangail'); //this returs a object
+
+console.log(amirKhan);
+console.log(salman);
+
+amirKhan.startSession();
+
+const ash = new NaptuneDev('Ash', 'Pak', 'Android');
+ash.realiseApp('v-1.4.3');
+// console.log(ash);
